7737e45459093a0962e419d7b0e886606a6aea4e
David Roundy <roundyd@physics.oregonstate.edu>
2015-09-26 09:28:40 -0400

    add pretty animation

--- src/main/java/org/abridgegame/randomwords/WordsActivity.java
+++ src/main/java/org/abridgegame/randomwords/WordsActivity.java
@@ -6,12 +6,20 @@ import android.view.View;
 import android.widget.TextView;
 import android.content.res.Resources;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+
 import java.util.Random;
 
 public class WordsActivity extends Activity
 {
     private Random random;
     private String[] words;
+    private TextView word;
+    private TextView old;
+
+    private int mAnimationDuration;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -21,14 +29,46 @@ public class WordsActivity extends Activity
         random = new Random();
         Resources res = getResources();
         words = res.getStringArray(R.array.words);
-        randomize(findViewById(R.id.word_text));
+        old = (TextView) findViewById(R.id.old_word_text);
+        word = (TextView) findViewById(R.id.word_text);
+        old.setVisibility(View.GONE);
+
+        // Retrieve and cache the system's default "short" animation time.
+        mAnimationDuration =
+            res.getInteger(android.R.integer.config_longAnimTime);
+
+        randomize(word);
     }
 
     /** Called when the user clicks the randomize button */
     public void randomize(View view) {
         // Pick a new random word!
-        TextView text = (TextView) findViewById(R.id.word_text);
         int r = random.nextInt(words.length);
-        text.setText(words[r]);
+        TextView temp = old;
+        old = word;
+        word = temp;
+
+        old.animate().alpha(0f)
+            .rotation(70)
+            .setDuration(mAnimationDuration)
+            .translationY(150)
+            .translationX(50)
+            .setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    old.setVisibility(View.GONE);
+                }
+            });
+
+        word.setText(words[r]);
+        word.setAlpha(0f);
+        word.setTranslationY(-100);
+        word.setTranslationX(0);
+        word.setRotation(0);
+        word.setVisibility(View.VISIBLE);
+        word.animate().alpha(1f)
+            .translationY(0)
+            .setDuration(2*mAnimationDuration).setListener(null);
+
     }
 }
