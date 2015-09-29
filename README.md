d5cb8a68bc1d670ced30eec8b095f905f6532422
David Roundy <roundyd@physics.oregonstate.edu>
2015-09-29 11:11:38 -0400

    add README

---
+++ README.md
@@ -0,0 +1,8 @@
+# Random Words
+
+This is a very simple android app that picks random words for any
+parlor game that involves getting people to guess the word: charades,
+drawing clues, giving verbal clues but not using the word, etc.
+
+The words and phrases are selected from the most commonly used words
+and the most commonly visited Wikipedia page titles.
--- mkwords.py
+++ mkwords.py
@@ -1,4 +1,4 @@
-import urllib2, re
+import urllib2, re, subprocess
 
 wikitop = re.findall(r'>([^<]+)</a></td>',
                      urllib2.urlopen("http://stats.grok.se/en/top").read())
@@ -20,35 +20,19 @@ with open("google-10000-english-usa.txt", 'r') as content_file:
 with open("/usr/share/dict/american-english-small", 'r') as content_file:
     usr_share = content_file.read().split();
 
-words = [val for val in google if val in usr_share]
+def is_noun_or_etc(w):
+    try:
+        definition = subprocess.check_output(["dict","-f",'-C',"-d","wn",w])
+        return ' n ' in definition or ' v ' in definition or ' adj ' in definition
+    except:
+        return False
+
+words = [val for val in google if val in usr_share and is_noun_or_etc(val)]
 
 def is_okay(w):
     return not "'" in w
 
 words = filter(is_okay, words)
-
-for w in usr_share:
-    if w + 's' in words:
-        words.remove(w + 's')
-    if w + 'ing' in words:
-        words.remove(w + 'ing')
-    if w + 'ed' in words:
-        words.remove(w + 'ed')
-    if w + 'ly' in words:
-        words.remove(w + 'ly')
-    if w[-1] == 'y' and w[:-1] + 'ily' in words:
-        words.remove(w[:-1] + 'ily')
-    if w[-1] == 'e':
-        if w[:-1] + 'ing' in words:
-            words.remove(w[:-1] + 'ing')
-        if w[:-1] + 'ed' in words:
-            words.remove(w[:-1] + 'ed')
-    if w[-1] == 'm':
-        if w + 'ming' in words:
-            words.remove(w + 'ming')
-        if w + 'med' in words:
-            words.remove(w + 'med')
-
 
 print """<?xml version="1.0" encoding="utf-8"?>
 <resources>
